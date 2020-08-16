6340227ffcc752933df772007069a753eac56648
Jeffrey Harding <jeffreyh202@gmail.com>
2020-08-15 21:10:48 -0600

    bones

---
+++ 02-Homework/Develop/schema.sql
@@ -0,0 +1,30 @@
+--Create Employee Database
+CREATE DATABASE employees_db;
+USE employees_db;
+
+--Create Departments Table
+CREATE TABLE department (
+name VARCHAR(30) NOT NULL,
+id INTEGER (11),
+PRIMARY KEY (id)
+);
+
+--Create Role Table
+CREATE TABLE roles (
+title VARCHAR (30) NOT NULL,
+salary DECIMAL (10,2) NULL,
+department_id INT,
+id INTEGER (10),
+PRIMARY KEY (id)
+);
+
+--Create Employee Table
+CREATE TABLE employees (
+first_name VARCHAR(30) NOT NULL,
+surname VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+id INTEGER(10),
+PRIMARY KEY (id)
+);
+
